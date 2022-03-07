e130b312fb1cea197a5718070fb533e5bf4b03a4
Victor Forissier <34337148+vforissi@users.noreply.github.com>
2022-03-07 13:23:11 -0500

    💩

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
+	<div id="app">
+		<!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
-  </div>
+    <router-view/> -->
+		Hacking away <a href="https://narcissa.xyz" target="_blank">Narcissa</a>, a
+		protocol to help crypto projects build with their community and contribute
+		to web3 open-source. <br /><br />You can find me
+		<a href="https://twitter.com/jacobinguy" target="_blank">here</a> on
+		Twitter.
+	</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: left;
+	max-width: 420px;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a {
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
